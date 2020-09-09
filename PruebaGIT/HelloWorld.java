34f70aa3f778d03220ad46b1cbb4f5a90cdf27f4
Cesar <cesarinpao10@gmail.com>
2020-09-09 05:29:12 -0500

    first commit

--- PruebaGIT/HelloWorld.java
+++ PruebaGIT/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo//Prueba conexion con GIT");
+		System.out.println("EXITOSO......!!!");
 	}
 
 }
